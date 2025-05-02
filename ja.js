ce05180195f10756d82377ea8335c3d8a7079a3d
Jurrair <m.jurrairfreelancer66@gmail.com>
2025-05-02 16:31:47 +0500

    I have design the clock in the css

---
+++ ja.js
@@ -0,0 +1,5 @@
+let display =  document.querySelector(".display");
+let stop    =  document.querySelector(".box1");
+let start    = document.querySelector(".box2");
+let reset    = document.querySelector(".box3");
+
